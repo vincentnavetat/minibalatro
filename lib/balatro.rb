7e7481f0a6f47edf21a5f9b9aa47e506011a3a97
Vincent Navetat <vincentnavetat@gmail.com>
2025-11-01 21:03:23 +0000

    Move deck to separate class

--- lib/balatro.rb
+++ lib/balatro.rb
@@ -1,12 +1,26 @@
 require 'pry'
 
+class Deck
+  attr_reader :cards
+  extend Forwardable
+  def_delegators :@cards, :size, :shift
+  include Enumerable
+
+  def initialize
+    @cards = ["H", "D", "C", "S"]
+      .map { |colour| [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map { |number| "#{colour}#{number}" } }
+      .flatten!
+      .shuffle
+  end
+end
+
 class Balatro
   attr_reader :deck, :hand
 
   HAND_SIZE = 7
 
   def initialize
-    @deck = initialize_deck
+    @deck = Deck.new
     @hand = pick_hand
   end
 
@@ -17,13 +31,6 @@ class Balatro
   end
 
   private
-
-  def initialize_deck
-    ["H", "D", "C", "S"]
-      .map { |colour| [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map { |number| "#{colour}#{number}" } }
-      .flatten!
-      .shuffle
-  end
 
   def pick_hand
     h = []
